25fbc3bcaada540cdd34a0bcdce9f98194a67bb5
lukas <luk.schild@t-online.de>
2020-03-04 08:37:26 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/ense3/tripit/trip_it_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ense3.tripit.trip_it_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
