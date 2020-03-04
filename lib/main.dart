6d487c7cdd6a0c917fafb6353a0833b44163e6d0
Steven Le Pollotec <steven.lepollotec@gmail.com>
2020-03-04 13:20:36 +0100

    Theme pp=our l'application

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+
+      return MaterialApp(
+        title: 'Trip-it',
+  theme: ThemeData(
+    // Define the default brightness and colors.
+    brightness: Brightness.light,
+    primaryColor: Color.fromRGBO(32, 158, 187, 1.0),
+    accentColor: Color.fromRGBO(2, 48, 71, 1.0),
+
+    // Define the default font family.
+    fontFamily: 'Roboto',
+
+    // Define the default TextTheme. Use this to specify the default
+    // text styling for headlines, titles, bodies of text, and more.
+    textTheme: TextTheme(
+      headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+      title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+      body1: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
+            
+    ),
+  ),
+
+   home: MyHomePage(title: 'Trip-it !'),
     );
   }
 }
@@ -92,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Tapez le bouton !',
             ),
             Text(
               '$_counter',
