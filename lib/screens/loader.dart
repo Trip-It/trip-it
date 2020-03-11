d08acca05fe266bed7085aad2bd34f1cdead889f
anasd <anas.dehmani@gmail.com>
2020-03-11 14:03:54 +0100

    Loading animation. (You can also change the colors so they match the theme) You can see how to use it on the example TestScreen on TI-9-LoadingScreen.

--- lib/screens/loader.dart
+++ lib/screens/loader.dart
@@ -161,28 +161,3 @@ class Dot extends StatelessWidget {
     );
   }
 }
-
-class Dialogs {
-  static Future<void> showLoadingDialog(
-      BuildContext context, GlobalKey key) async {
-    return showDialog<void>(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return new WillPopScope(
-              onWillPop: () async => false,
-              child: SimpleDialog(
-                  key: key,
-                  backgroundColor: Colors.black54,
-                  children: <Widget>[
-                    Center(
-                      child: Column(children: [
-                        Loader(),
-                        SizedBox(height: 10,),
-                        Text("Please Wait....",style: TextStyle(color: Colors.blueAccent),)
-                      ]),
-                    )
-                  ]));
-        });
-  }
-}
