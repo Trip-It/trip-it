c3ae768f44af624b6fab7deacae88c4e24d8557b
lukas <luk.schild@t-online.de>
2020-04-08 21:24:53 +0200

    Adapted home page, added SearchBar utils

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,74 +1,73 @@
-import 'dart:async';
-import 'package:flutter/cupertino.dart';
+///
+///  License
+///  Apache License 2.0
+///  Copyright (c) 2020 Aliati Sotware, Lucas Finoti and João Pedro Martins
+///
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
-import 'package:user_location/user_location.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
-/// Map widget using Mapbox API
-/// with [followUser] you can set whether or not the map should stay centered
-/// on the most recent user location or not
-class MapView extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  MapPage({
+    Key key,
+    @required this.lat,
+    @required this.lng,
+    @required this.mapController,
+    @required this.markers,
+    this.isNominatim = true,
+    this.apiKey,
+    this.customMapLayer,
+  }) : super(key: key);
+  final List<Marker> markers;
+  final double lat;
+  final double lng;
+  final MapController mapController;
+  final bool isNominatim;
+  final String apiKey;
+  final TileLayerOptions customMapLayer;
 
-  ///Map controller to manage app
-  MapController mapController = MapController();
-  UserLocationOptions userLocationOptions;
-  StreamController<LatLng> markerlocationStream = StreamController();
-  List<Marker> markers = [];
-  LatLng position = LatLng(45.171547, 5.722387);
-  bool followUser;
-
-  /// Constructor to initialize widget, [followUser] sets if map stays centered
-  /// on user location or not
-  MapView(this.followUser,this.position);
-
-  GlobalKey<OSMFlutterState> mapKey = GlobalKey<OSMFlutterState>();
   @override
-  Widget build(BuildContext context) {
-    markerlocationStream.stream.listen((onData) {
-      // print(onData.latitude);
-    });
+  _MapPageState createState() => _MapPageState();
+}
 
-    userLocationOptions = UserLocationOptions(
-        context: context,
-        mapController: mapController,
-        markers: markers,
-        onLocationUpdate: (LatLng pos) =>
-            position = pos,
-        updateMapLocationOnPositionChange: false,
-        showMoveToCurrentLocationFloatingActionButton: false,
-        zoomToCurrentLocationOnLoad: false,
-        fabBottom: 30,
-        fabRight: 10,
-        verbose: false);
-
-    return Container(
-      child: Column(
-        children: [
-          Flexible(
-            child: OSMFlutter(
-              key: mapKey,
-              currentLocation: true,
-              road: Road(
-                  startIcon: MarkerIcon(
-                    icon: Icon(
-                      Icons.person,
-                      size: 64,
-                    ),
-                  ),
-                  roadColor: Colors.blueAccent),
-              markerIcon: MarkerIcon(
-                icon: Icon(
-                  Icons.person_pin_circle,
-                  size: 56,
-                ),
-              ),
-            ),
-          ),
-        ],
+class _MapPageState extends State<MapPage> {
+  Widget body(BuildContext context) {
+    return new FlutterMap(
+      mapController: widget.mapController,
+      options: MapOptions(
+        center: LatLng(widget.lat, widget.lng),
+        zoom: 13.0,
       ),
+      layers: [
+        widget.isNominatim
+            ? widget.customMapLayer == null
+            ? new TileLayerOptions(
+            urlTemplate:
+            'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'])
+            : widget.customMapLayer
+            : widget.customMapLayer == null
+            ? new TileLayerOptions(
+          urlTemplate: "https://api.tiles.mapbox.com/v4/"
+              "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+          additionalOptions: {
+            'accessToken': widget.apiKey,
+            'id': 'mapbox.streets',
+          },
+        )
+            : widget.customMapLayer,
+        MarkerLayerOptions(
+          markers: widget.markers,
+        ),
+      ],
     );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: body(context),
+    );
+  }
 }
