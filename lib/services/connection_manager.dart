586048b6b02d30adb2e32435c7dbccd17c715c03
simonfi1337 <simon.fichtner@googlemail.com>
2020-04-17 00:07:25 +0200

    added service class connection_manager

--- lib/services/connection_manager.dart
+++ lib/services/connection_manager.dart
@@ -2,21 +2,61 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
-class BluetoothHandler{
+class ConnectionManager {
+  FlutterBlue flutterBlue;
+  List<BluetoothDevice> devicesList;
+  Map<Guid, List<int>> readValues;
+  BluetoothDevice _connectedDevice;
+  List<BluetoothService> _services;
 
-  FlutterBlue flutterBlue = FlutterBlue.instance;
+  connectionManager() {
+    this.flutterBlue = FlutterBlue.instance;
+    this.devicesList = new List<BluetoothDevice>();
+    this.readValues = new Map<Guid, List<int>>();
+    this._services = new List<BluetoothService>();
+  }
 
-  void scan() {
-
-    var results = flutterBlue.scan(timeout: Duration(seconds: 4));
-
-    var subscription = flutterBlue.scanResults.listen((results){
-      for (ScanResult r in results){
-        print('${r.device.name} found! rssi: ${r.rssi}');
+  void fillDeviceList(){
+    flutterBlue.startScan();
+    flutterBlue.connectedDevices
+        .asStream()
+        .listen((List<BluetoothDevice> devices) {
+      for (BluetoothDevice device in devices) {
+        addDeviceToList(device);
       }
     });
+    flutterBlue.scanResults.listen((List<ScanResult> results) {
+      for (ScanResult result in results) {
+        addDeviceToList(result.device);
+      }
+    });
+  }
 
+  void addDeviceToList(final BluetoothDevice device) {
+    if (!devicesList.contains(device)) {
+      devicesList.add(device);
+    }
+    ;
+  }
+
+  void startScanDev() {
+    flutterBlue.startScan();
+  }
+
+  void stopScanDev() {
     flutterBlue.stopScan();
   }
-}
 
+  void discoverServicesDev(BluetoothDevice device) async {
+    try {
+      await device.connect();
+    } catch (e) {
+      if (e.code != 'already_connected') {
+        throw e;
+      }
+    } finally {
+      _services = await device.discoverServices();
+    }
+    _connectedDevice = device;
+  }
+}
