92087cb6d07b41b5cc57059b0f2307ad8cd73a4a
simonfi1337 <simon.fichtner@googlemail.com>
2020-05-01 22:07:43 +0200

    changed name of databasehandler file

--- lib/services/obd_database_handler.dart
+++ lib/services/obd_database_handler.dart
@@ -1,112 +1,152 @@
-import 'package:flutter_blue/flutter_blue.dart';
 import 'package:trip_it_app/services/database_manager.dart';
+import 'package:trip_it_app/models/obd_service.dart';
+import 'package:trip_it_app/models/car_state.dart';
+import 'dart:async';
+import 'package:csv/csv.dart';
 import 'package:path/path.dart';
 import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:trip_it_app/models/car_state.dart';
+import 'package:trip_it_app/models/obd_service.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:typed_data';
-import 'package:trip_it_app/models/obd_service.dart';
-import 'package:trip_it_app/services/connection_manager.dart';
-import 'package:trip_it_app/screens/obd_data.dart';
-import 'package:flutter/foundation.dart';
+
 
 class ObdDatabaseHandler extends DatabaseManager {
-  List<BluetoothService> realServices;
-  List<ObdService> realObdServices;
-
-
   ///init ObdDatabaseHandler
-  @override
+
   ObdDatabaseHandler() : super();
 
-  @override
-  initDB() async {
-    // init the database tripitDB with one attribute id
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, "tripitDB");
-    return await openDatabase(path, version: 1, onOpen: (db) async {
-      // Only copy if the database doesn't exist
-      if (FileSystemEntity.typeSync(path) == FileSystemEntityType.notFound) {
-        // Load database from asset and copy
-        ByteData data = await rootBundle.load(join('data', 'trip_it_data.db'));
-        List<int> bytes =
-            data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-
-        // Save copied asset to documents
-        await new File(path).writeAsBytes(bytes);
-      }
-
-      var db = await openDatabase(path);
-    }, onCreate: (Database db, int version) async {
-      await db.execute('''
-create table TableService (
-  ID integer primary key autoincrement,
-  UUID var,
-  Characteristics List<BluetoothCharacteristic> not null)
-''');
-
-//              "CREATE TABLE ObdServices(uuid VAR, service BLUETOOTHSERVICE, characteristics LIST<BLUETOOTHCHARACTERISTIC> )");
-      // Use the following line to create new tables
-      //await db.execute("CREATE TABLE TableName(attribute TYPE)");
-    });
+  //get all CarStates in Database
+  Future<List<CarState>> getCarState() async {
+    var dbClient = await database;
+    List<Map> list = await dbClient.rawQuery('SELECT * FROM carstates');
+    List<CarState> carStates = new List();
+    for (int i = 0; i < list.length; i++) {
+      carStates.add(new CarState(
+          list[i]["id"], list[i]["speed"], list[i]["soc"], list[i]["soh"]));
+    }
+    return carStates;
   }
 
-
-  //insert obdService into Database db for obdServices
-  Future<void> insertObdService(ObdService obdService) async {
-    final Database db = await database;
-    await db.insert('obdServices', obdService.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-
-  ///get obdService from Database db
+  //get all ObdServices in Database
   Future<List<ObdService>> getObdService() async {
-    // Get a reference to the database.
-    final Database db = await database;
+    var dbClient = await database;
+    List<Map> list = await dbClient.rawQuery('SELECT * FROM obdservices');
+    List<ObdService> obdServices = new List();
+    for (int i = 0; i < list.length; i++) {
+      obdServices.add(new ObdService()); //list[i]["uuid"], list[i]["uuidchar"]
+    }
+    return obdServices;
+  }
 
-    // Query the table for all The obdServices.
-    final List<Map<String, dynamic>> maps = await db.query('obdService');
+  //get CarState of id X
+  Future<CarState> getSingleCarState(int id) async {
+    var dbClient = await database;
+    List<Map> carState =
+    await dbClient.rawQuery('SELECT * FROM carstates WHERE id = ?', [id]);
 
-    // Convert the List<Map<String, dynamic> into a List<obdService>.
-    return List.generate(maps.length, (i) {
-      return ObdService(
-        id : maps[i]['id'],
-        uuid: maps[i]['uuid'],
-//        service: maps[i]['service'],
-        characteristics: maps[i]['characteristics'],
-      );
+    if (carState.isEmpty) {
+      return null;
+    } else {
+      return new CarState(carState.first["id"], carState.first["speed"],
+          carState.first["soc"], carState.first["soh"]);
+    }
+  }
+
+  void saveCarState(CarState carState) async {
+    var dbClient = await database;
+
+    // Check if carState is already existing
+    final check = await this.getSingleCarState(carState.id);
+
+    if (check == null) {
+      await dbClient.transaction((txn) async {
+        return await txn.rawInsert(
+            'INSERT INTO carstates(id, speed, soc, soh) VALUES(' +
+                '\'' +
+                carState.id.toString() +
+                '\'' +
+                ',' +
+                '\'' +
+                carState.speed.toString() +
+                '\'' +
+                ',' +
+                '\'' +
+                carState.soc.toString() +
+                '\'' +
+                ',' +
+                '\'' +
+                carState.soh.toString() +
+                '\'' +
+                ')');
+      });
+    }
+    return;
+  }
+
+  //delete one carstate with id X
+  Future<bool> deleteCarState(CarState carState) async {
+    var dbClient = await database;
+
+    int res = await dbClient
+        .rawDelete('DELETE FROM carstates WHERE id = ?', [carState.id]);
+    return res > 0 ? true : false;
+  }
+
+  //empty table carstates
+  Future<void> emptyCarStates() async {
+    var dbClient = await database;
+    dbClient.execute('DELETE FROM carstates');
+    dbClient.execute('VACUUM');
+  }
+
+  Future<void> startRecording(Timer timerSaving, savingInterval,
+      CarState carState) async {
+    Timer.periodic(savingInterval, (timerSaving) {
+      this.saveCarState(carState);
     });
   }
 
-  Future<void> updateObdService(ObdService obdService) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Update the given ObdService.
-    await db.update(
-      'obdServices',
-      obdService.toMap(),
-      // Ensure that the Service has a matching id.
-      where: "id = ?",
-      // Pass the obdServices's id as a whereArg to prevent SQL injection.
-      whereArgs: [obdService.id],
-    );
+  Future<void> stopRecording(Timer timerSaving) async {
+    timerSaving.cancel();
   }
 
-  Future<void> deleteObdService(int id) async {
-    // Get a reference to the database.
-    final db = await database;
+  Future<void> exportToCSV() async {
+    Future<List<CarState>> list = getCarState();
+    List<CarState> carStateList = new List();
+    carStateList = await list;
 
-    // Remove the obdService from the Database.
-    await db.delete(
-      'obdServices',
-      // Use a `where` clause to delete a specific ObdService.
-      where: "id = ?",
-      // Pass the obdServices' id as a whereArg to prevent SQL injection.
-      whereArgs: [id],
-    );
+    List<List<dynamic>> rows = List<List<dynamic>>();
+    for (int i = 0; i < carStateList.length; i++) {
+//row refer to each column of a row in csv file and rows refer to each row in a file
+      List<dynamic> row = List();
+      row.add(carStateList[i].id);
+      row.add(carStateList[i].speed);
+      row.add(carStateList[i].soc);
+      row.add(carStateList[i].soh);
+      rows.add(row);
+    }
+
+//store file in documents folder
+    String dir = (await getExternalStorageDirectory()).absolute.path +
+        "/documents";
+    String file = "$dir";
+    File f = new File(file + "obddata.csv");
+
+// convert rows to String and write as csv file
+    String csv = const ListToCsvConverter().convert(rows);
+    f.writeAsString(csv);
   }
+
+  /// Missing implementation of DatabaseHandler for ObdService
+  /// Difficult because of List of Characteristics and Datatype: GUID
+
+// In order to add all characteristics the columns need to be enhanced according to the amount of characteristics per service
+// Column1: Service UUID, Column 2: Characteristic[1], Column 3: ..., Column N: Characteristic[N-1]
+  addColumnsToTable() async {
+    var dbClient = await database;
+  }
+
 }
-
 
