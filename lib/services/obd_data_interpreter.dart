8f76b9bb9ebe1d7ea8d18bad3991a946cb2ff744
simonfi1337 <simon.fichtner@googlemail.com>
2020-05-10 10:52:40 +0200

    class obdDataInterpreter

---
+++ lib/services/obd_data_interpreter.dart
@@ -0,0 +1,11 @@
+import 'package:trip_it_app/models/car_state.dart';
+import 'package:trip_it_app/models/car.dart';
+import 'package:trip_it_app/models/obd_service.dart';
+
+
+class ObdDataInterpreter {
+
+  String carName;
+  ObdDataInterpreter({this.carName});
+
+}
