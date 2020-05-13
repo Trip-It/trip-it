d887eda32ea4fec6ac2313732f41d377e35afeb9
simonfi1337 <simon.fichtner@googlemail.com>
2020-05-13 15:44:46 +0200

    update translator

--- lib/services/obd_data_interpreter.dart
+++ lib/services/obd_data_interpreter.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_blue/flutter_blue.dart';
 import 'package:trip_it_app/models/car_state.dart';
 import 'package:trip_it_app/models/obd_service.dart';
 import 'package:trip_it_app/models/protocol.dart';
@@ -28,6 +29,10 @@ class ObdDataInterpreter {
         carProtocol = null; //assign protocol typ Zoe R90 22kWh
         break;
     }}
+
+    List<BluetoothService> filterServices(BluetoothService services){
+    // use necessary SIDs from protocol to filter retrieved data from OBD in order to get only the necessary data for car state
+    }
 
   CarState translateService(ObdService obdService, Protocol protocol){
   CarState carState;
