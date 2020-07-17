0f83486d9da99b99c1b2d83f33cc094d2e6e499b
Lukas Schild <lukas.schild@grenoble-inp.org>
2020-07-17 16:54:24 +0200

    Added ProfileWidget class to hotfix the access problem of the current Profile for the driving context used in the consumption algorithm

---
+++ lib/widgets/profile_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:trip_it_app/models/profile.dart';
+
+class ProfileWidget extends StatelessWidget{
+
+  final Profile profile;
+
+  ProfileWidget(this.profile);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+
+}
